33d56b9d9d55e3d7cbbe82ee9ce554bdfeec4f3a
NatasjaLoevschall <natatasja.loevschall@gmail.com>
2025-10-06 10:06:51 +0200

    java ændringer

--- scriptforside.js
+++ scriptforside.js
@@ -1,4 +1,14 @@
+//tager fat i overskriften
 const overskrift = document.getElementById("stjernensliv");
+
+window.onload = function() { //window.onload sørger for at koden først kører når siden er loadet
+  setTimeout(() => {
+    overskrift.style.opacity = 1;
+  }, 500); // 1000 ms = 1 sekund
+};
+
+//tager fat i knappen
+const knap = document.querySelector("#forside a");
 
 window.onload = function() { //window.onload sørger for at koden først kører når siden er loadet
   setTimeout(() => {
